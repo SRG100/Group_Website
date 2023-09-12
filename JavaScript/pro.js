5a913c3881b1952fae544901d69d990bb0f0a132
Shreyash <shreyashghal3@gmail.com>
2023-09-12 21:53:13 +0545

    first commit

---
+++ JavaScript/pro.js
@@ -0,0 +1,127 @@
+var i = 0;
+
+function OpenPopup() {
+    // JS for alert message to appear 
+    alert("Your item has been added to cart!!!")
+    // for the number of items in add to increase every time Add to cart button is clicked
+    document.getElementById("ATC").style.visibility = "visible";
+    i++;
+    document.getElementById("ATC").innerHTML = "<h3>" + i + "</h3>"
+}
+
+//To open and close product details
+function opendetails1() {
+    document.getElementById("DET1").classList.add("showdetails");
+}
+function closedetails1() {
+    document.getElementById("DET1").classList.remove("showdetails");
+}
+function opendetails2() {
+    document.getElementById("DET2").classList.add("showdetails");
+}
+function closedetails2() {
+    document.getElementById("DET2").classList.remove("showdetails");
+}
+function opendetails3() {
+    document.getElementById("DET3").classList.add("showdetails");
+}
+function closedetails3() {
+    document.getElementById("DET3").classList.remove("showdetails");
+}
+function opendetails4() {
+    document.getElementById("DET4").classList.add("showdetails");
+}
+function closedetails4() {
+    document.getElementById("DET4").classList.remove("showdetails");
+}
+function opendetails5() {
+    document.getElementById("DET5").classList.add("showdetails");
+}
+function closedetails5() {
+    document.getElementById("DET5").classList.remove("showdetails");
+}
+//
+function opendetails6() {
+    document.getElementById("DET6").classList.add("showdetails");
+}
+function closedetails6() {
+    document.getElementById("DET6").classList.remove("showdetails");
+}
+//
+function opendetails7() {
+    document.getElementById("DET7").classList.add("showdetails");
+}
+function closedetails7() {
+    document.getElementById("DET7").classList.remove("showdetails");
+}
+//
+function opendetails8() {
+    document.getElementById("DET8").classList.add("showdetails");
+}
+function closedetails8() {
+    document.getElementById("DET8").classList.remove("showdetails");
+}
+//
+function opendetails9() {
+    document.getElementById("DET9").classList.add("showdetails");
+}
+function closedetails9() {
+    document.getElementById("DET9").classList.remove("showdetails");
+}
+//
+function opendetails10() {
+    document.getElementById("DET10").classList.add("showdetails");
+}
+function closedetails10() {
+    document.getElementById("DET10").classList.remove("showdetails");
+}
+//
+
+function opendetails11() {
+    document.getElementById("DET11").classList.add("showdetails");
+}
+function closedetails11() {
+    document.getElementById("DET11").classList.remove("showdetails");
+}
+//
+
+function opendetails12() {
+    document.getElementById("DET12").classList.add("showdetails");
+}
+function closedetails12() {
+    document.getElementById("DET12").classList.remove("showdetails");
+}
+//
+function opendetails13() {
+    document.getElementById("DET13").classList.add("showdetails");
+}
+function closedetails13() {
+    document.getElementById("DET13").classList.remove("showdetails");
+}
+
+//
+function opendetails14() {
+    document.getElementById("DET14").classList.add("showdetails");
+}
+function closedetails14() {
+    document.getElementById("DET14").classList.remove("showdetails");
+}
+//
+
+function opendetails15() {
+    document.getElementById("DET15").classList.add("showdetails");
+}
+function closedetails15() {
+    document.getElementById("DET15").classList.remove("showdetails");
+}
+//
+
+function opendetails16() {
+    document.getElementById("DET16").classList.add("showdetails");
+}
+function closedetails16() {
+    document.getElementById("DET16").classList.remove("showdetails");
+}
+
+
+
